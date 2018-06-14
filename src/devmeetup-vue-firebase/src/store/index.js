422cc804f3c42825073e717c57e64e01f36aad61
motion2015 <motion74@naver.com>
2018-06-14 22:47:40 +0900

    upload

---
+++ src/devmeetup-vue-firebase/src/store/index.js
@@ -0,0 +1,67 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export const store = new Vuex.Store({
+  state: {
+    loadedMeetups: [
+      {
+        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/4/47/New_york_times_square-terabass.jpg',
+        id: 'afajfjadfaadfa323',
+        title: 'Meetup in New York',
+        date: new Date(),
+        location: 'New York',
+        description: 'New York, New York!'
+      },
+      {
+        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/7/7a/Paris_-_Blick_vom_gro%C3%9Fen_Triumphbogen.jpg',
+        id: 'aadsfhbkhlk1241',
+        title: 'Meetup in Paris',
+        date: new Date(),
+        location: 'Paris',
+        description: 'It\'s Paris!'
+      }
+    ],
+    user: {
+      id: 'ajaldslfalsk12',
+      registeredMeetups: ['aadsfhbkhlk1241']
+    }
+  },
+  mutations: {
+    createMeetup (state, payload) {
+      state.loadedMeetups.push(payload)
+    }
+  },
+  actions: {
+    createMeetup ({commit}, payload) {
+      const meetup = {
+        title: payload.title,
+        location: payload.location,
+        imageUrl: payload.imageUrl,
+        description: payload.description,
+        date: payload.date,
+        id: 'kfdlsfjslakl12'
+      }
+      // Reach out to firebase and store it
+      commit('createMeetup', meetup)
+    }
+  },
+  getters: {
+    loadedMeetups (state) {
+      return state.loadedMeetups.sort((meetupA, meetupB) => {
+        return meetupA.date > meetupB.date
+      })
+    },
+    featuredMeetups (state, getters) {
+      return getters.loadedMeetups.slice(0, 5)
+    },
+    loadedMeetup (state) {
+      return (meetupId) => {
+        return state.loadedMeetups.find((meetup) => {
+          return meetup.id === meetupId
+        })
+      }
+    }
+  }
+})
